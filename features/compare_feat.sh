25cf28536b2ee37862866cadbaf8b6a946127d00
ZhiYeG <861272076@qq.com>
2019-08-14 12:30:53 -0500

    update setup feature compare

--- features/compare_feat.sh
+++ features/compare_feat.sh
@@ -5,9 +5,9 @@ global_dir=/data/wuti/DNCON4/test/DNCON4_system
 source $global_dir/env/dncon4_virenv/bin/activate
 output_dir=$global_dir/example/T0771/
 input_dir=$global_dir/features/T0771/
-fasta=$global_dir/example/T0771/T0771-D1.pdb
+pdb=$global_dir/example/T0771/T0771-D1.pdb
 target=T0771
 printf "$global_dir\n"
 
 #################CV_dir output_dir dataset database_path
-python $global_dir/scripts/dncon4.py $feature_dir $fasta $output_dir
+python $global_dir/scripts/compare_feat.py $target $pdb $input_dir $output_dir
---
+++ features/readme.txt
@@ -0,0 +1,9 @@
+For testing features 
+
+cd DNCON4_system/features
+
+1.  Run feature generation script
+sh generate_feat.sh
+
+2. Compare feature you generated('DNCON4_system/features/T0771') with example(DNCON4_system/example/T0771)
+sh compare_feat.sh
